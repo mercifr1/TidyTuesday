d150eafa159fbf009ebee6e78de73638d395839e
Francois Mercier <francois0mercier@gmail.com>
2020-04-29 19:59:14 +0200

    Final Broadway show

--- 2020Wk18- Broadway/TidyTuesday-2020Wk18-Broadway.R
+++ 2020Wk18- Broadway/TidyTuesday-2020Wk18-Broadway.R
@@ -13,8 +13,10 @@
 #' --------------------------------------------------
 library(tidyverse)
 library(lubridate)
+library(patchwork)
 library(scales)
 library(extrafont)
+sessionInfo()
 
 
 #' 1. Import and cleanup a bit the raw data
@@ -76,9 +78,107 @@ real_grosses<-grosses_clean %>%
   select(-year_month:-jan_2020_dollars)
 
 
-#' 2. Import and cleanup a bit the raw data
+
+#' 2. Derive cumulative gross by week
 #' --------------------------------------------------
 
+cumgross<-real_grosses %>%
+  filter(run_number==1) %>%
+  group_by(show, run_number) %>%
+  mutate(
+    # Use coalesce() for shows that have some NAs
+    weekly_gross=coalesce(weekly_gross, 0),
+    cumulative_gross=cumsum(weekly_gross),
+    show_label=ifelse(week_ending==max(week_ending), paste0(" ", show), NA_character_)
+  ) %>%
+  ungroup() %>%
+  mutate(show_and_run=paste(show, run_number))
+
+cumgross %>% filter(show=="The Phantom of the Opera") %>%
+  filter(row_number() %in% c(1, n())) %>%
+  select(show, week_ending, week_of_run, cumulative_gross) 
+
+#' 4. Explore a little
+#' --------------------------------------------------
+#' Initial graph
+wrapper<-function(x, ...) paste(strwrap(x, ...), collapse = "\n")
+mytext<-c("The Phantom of the Opera has been running for more than 30 years and
+          has cumulated 1677 weeks of run.")
+dat_text<-data.frame(label=wrapper(mytext, width=40),
+                     x=ymd("2000-01-01"), y=1250*1e6)
+arrow_dat<-data.frame(x=ymd("2000-01-01"), xend=ymd("2002-06-01"), 
+                      y=1200*1e6, yend=900*1e6)
 
 
+p1<-cumgross %>%
+  drop_na(week_ending, cumulative_gross) %>%
+  ggplot(., aes(week_ending, cumulative_gross))+
+  #' geoms
+  geom_line(aes(group=show, colour=cumulative_gross), lwd=1)+
+  geom_text(data=dat_text, size=3.5, hjust=0, colour="white", 
+             mapping=aes(x=x, y=y, label=label))+
+  geom_curve(data=arrow_dat, aes(x=x, y=y, xend=xend, yend=yend),
+             inherit.aes=FALSE, colour="white",
+             arrow=arrow(length=unit(0.03, "npc"), type="closed",
+                         angle=c(20, 80)), curvature=-0.3)+
+  #' scales and coords
+  scale_y_continuous("", labels=label_dollar(scale=1/1e6, suffix="M"))+
+  scale_colour_viridis_c(option="inferno", direction=-1)+
+  coord_flip()+
+  #' labs
+  labs(x="", y="",
+       title="Eternal flames of Broadway Shows",
+       subtitle="Long lasting Broadway shows")+
+  #' themes
+  theme_dark()+
+  theme(legend.position="none", 
+        panel.grid.major=element_line(colour="grey62"), 
+        panel.grid.minor=element_line(colour="grey55"), 
+        panel.grid.major.y=element_blank(),   
+        panel.grid.minor.y=element_blank(), 
+        axis.text=element_text(colour="white"),
+        axis.ticks=element_line(colour="grey50"),
+        plot.background=element_rect(fill="grey50", color="grey50"),
+        plot.title=element_text(colour="white", hjust=0, size=16),
+        plot.subtitle=element_text(colour="white", hjust=0, size=10)) 
+p1
 
+p2<-cumgross %>%
+  group_by(show) %>%
+  arrange(cumulative_gross) %>%
+    slice(n()) %>%
+  ungroup() %>%
+  filter(cumulative_gross>500*1e6) %>%
+  arrange(cumulative_gross) %>%
+  mutate(topshow=factor(show)) %>%
+  mutate(toposhow=fct_inorder(topshow)) %>%
+  
+  ggplot(., aes(toposhow, cumulative_gross))+
+  #' geoms
+  geom_col(aes(fill=cumulative_gross))+
+  geom_text(aes(label=round(cumulative_gross/1e6,0),
+                y=cumulative_gross-50e6), 
+            color="white", size=3.5, hjust=1)+
+  #' scales and coords
+  scale_y_continuous("", labels=label_dollar(scale=1/1e6, suffix="M"))+
+  scale_fill_viridis_c(option="inferno", end=0.7, direction=-1)+
+  coord_flip()+
+  #' labs
+  labs(x="", y="",
+       subtitle="Shows with cumulative revenue >500 million USD",
+       caption=paste0("Data: Playbill | Data prep: @alexcookson"))+
+  theme_dark()+
+  theme(legend.position="none", 
+        axis.text.x=element_blank(),
+        axis.text.y=element_text(colour="white"),
+        axis.ticks=element_line(colour="grey50"),
+        panel.grid.major=element_blank(),   
+        panel.grid.minor=element_blank(), 
+        plot.background=element_rect(fill="grey50", color="grey50"),
+        plot.subtitle=element_text(colour="white", hjust=0, size=10),
+        plot.caption=element_text(colour="white"))
+
+pall<-p1/p2+
+  plot_layout(heights=c(3, 1))
+
+ggsave("Broadway.png", pall, width=9, height=10, dpi=300)
